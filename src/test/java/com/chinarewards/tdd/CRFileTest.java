829f37d4345944fc651ad6ae41b9952735538364
weishengshui <shengshui.wei@china-rewards.com>
2012-05-04 15:39:34 +0800

    Add one testCase to CRFileTest.java 	Fclose

--- src/test/java/com/chinarewards/tdd/CRFileTest.java
+++ src/test/java/com/chinarewards/tdd/CRFileTest.java
@@ -45,7 +45,7 @@ public class CRFileTest extends TestCase {
 		// 以写的方式打开一个不存在的文件，创建这个文件，获得一个文件对象
 		fileObject1 = (CRFile) file.fopen("file1", "w");
 		assertNotNull(fileObject1);
-		
+
 		String fileContents = "/2/12 1:33:44 PM CST: Eclipse is running in a JRE, but a JDK is required"
 				+ "Some Maven plugins may not work when importing projects or updating source folders."
 				+ "5/2/12 1:33:47 PM CST: Updating index central|http://repo1.maven.org/maven2"
@@ -61,70 +61,73 @@ public class CRFileTest extends TestCase {
 
 		// 往文件file1写入961个字节
 		assertEquals(961, file.fwrite(buffer, 1, buffer.length, fileObject1));
-		
+
 		// 以追加的方式打开文件file1，获得一个文件对象
 		fileObject1 = (CRFile) file.fopen("file1", "a");
 		assertNotNull(fileObject1);
-		//查看文件位置指针值
-		assertTrue(961==file.ftell(fileObject1));
+		// 查看文件位置指针值
+		assertTrue(961 == file.ftell(fileObject1));
 		// 以读的方式打开文件file1，得到一个文件对象
 		fileObject1 = (CRFile) file.fopen("file1", "r");
 		assertNotNull(fileObject1);
-		//查看文件位置指针值
-		assertTrue(0==file.ftell(fileObject1));
-		
-		// 以写的方式打开文件file1，此时将清空文件file1的内容
+		// 查看文件位置指针值
+		assertTrue(0 == file.ftell(fileObject1));
+
+		// 以写的方式打开文件file1，此时将清空文件file1的内容，文件长度为0
 		fileObject1 = (CRFile) file.fopen("file1", "w");
 		assertNotNull(fileObject1);
-		
+		//读取文件属性
+		Stat stat = new Stat();
+		assertTrue(0 == file.stat("file1", stat));
+		assertTrue(0 == stat.st_size);
 
 	}
 
-	// public void test_File_Stat_OK() {
-	// // format the disk and create a file object
-	// assertEquals(0, fs.format(true, 512));
-	// // 创建第一个文件
-	// CRFile fileObject1 = (CRFile) file.fopen("file1", "w");
-	// assertNotNull(fileObject1);
-	//
-	// // 查看文件的属性信息
-	// Stat stat = new Stat();
-	// assertEquals(0, file.stat("file1", stat));
-	// assertEquals(0, stat.st_size);
-	// assertEquals(0, stat.st_ino);
-	// assertEquals(512, stat.st_blksize);
-	// assertEquals(1, stat.st_blocks);
-	// SimpleDateFormat dateFormat = new SimpleDateFormat(
-	// "yyyy-MM-dd HH:mm:ss");
-	// System.out.println(dateFormat.format(new Date(stat.st_mtime)));
-	//
-	// try {
-	// Thread.sleep(10000);
-	// } catch (Exception e) {
-	// e.printStackTrace();
-	// }
-	// // 创建第二个文件
-	// fileObject1 = (CRFile) file.fopen("file2", "w");
-	// assertNotNull(fileObject1);
-	//
-	// // 查看文件的属性信息
-	// stat = new Stat();
-	// assertEquals(0, file.stat("file2", stat));
-	// assertEquals(0, stat.st_size);
-	// assertEquals(1, stat.st_ino);
-	// assertEquals(512, stat.st_blksize);
-	// assertEquals(1, stat.st_blocks);
-	// System.out.println(dateFormat.format(new Date(stat.st_mtime)));
-	//
-	// // 查看文件的属性信息
-	// stat = new Stat();
-	// assertEquals(0, file.stat("file1", stat));
-	// assertEquals(0, stat.st_size);
-	// assertEquals(0, stat.st_ino);
-	// assertEquals(512, stat.st_blksize);
-	// assertEquals(1, stat.st_blocks);
-	// System.out.println(dateFormat.format(new Date(stat.st_mtime)));
-	// }
+	 public void test_File_Stat_OK() {
+	 // format the disk and create a file object
+	 assertEquals(0, fs.format(true, 512));
+	 // 创建第一个文件
+	 CRFile fileObject1 = (CRFile) file.fopen("file1", "w");
+	 assertNotNull(fileObject1);
+	
+	 // 查看文件的属性信息
+	 Stat stat = new Stat();
+	 assertEquals(0, file.stat("file1", stat));
+	 assertEquals(0, stat.st_size);
+	 assertEquals(0, stat.st_ino);
+	 assertEquals(512, stat.st_blksize);
+	 assertEquals(1, stat.st_blocks);
+	 SimpleDateFormat dateFormat = new SimpleDateFormat(
+	 "yyyy-MM-dd HH:mm:ss");
+	 System.out.println(dateFormat.format(new Date(stat.st_mtime)));
+	
+	 try {
+	 Thread.sleep(10000);
+	 } catch (Exception e) {
+	 e.printStackTrace();
+	 }
+	 // 创建第二个文件
+	 fileObject1 = (CRFile) file.fopen("file2", "w");
+	 assertNotNull(fileObject1);
+	
+	 // 查看文件的属性信息
+	 stat = new Stat();
+	 assertEquals(0, file.stat("file2", stat));
+	 assertEquals(0, stat.st_size);
+	 assertEquals(1, stat.st_ino);
+	 assertEquals(512, stat.st_blksize);
+	 assertEquals(1, stat.st_blocks);
+	 System.out.println(dateFormat.format(new Date(stat.st_mtime)));
+	
+	 // 查看文件的属性信息
+	 stat = new Stat();
+	 assertEquals(0, file.stat("file1", stat));
+	 assertEquals(0, stat.st_size);
+	 assertEquals(0, stat.st_ino);
+	 assertEquals(512, stat.st_blksize);
+	 assertEquals(1, stat.st_blocks);
+	 System.out.println(dateFormat.format(new Date(stat.st_mtime)));
+	 }
 
 	public void test_File_Fwrite_Fseek_Fread() {
 		// format the disk and create a file object
@@ -305,6 +308,54 @@ public class CRFileTest extends TestCase {
 
 	}
 
+	//关闭文件测试
+	public void test_File_Fclose() {
+		
+		// format the disk and create a file object
+		assertEquals(0, fs.format(true, 512));
+		
+		//关闭一个空对象
+		assertTrue(CRFile.EOF==file.fclose(null));
+		
+		
+		// 创建文件"file1"
+		CRFile fileObject1 = (CRFile) file.fopen("file1", "w");
+		assertNotNull(fileObject1);
+		// 查看文件位置指针
+		assertEquals(0, file.ftell(fileObject1));
+		String fileContents = "/2/12 1:33:44 PM CST: Eclipse is running in a JRE, but a JDK is required"
+				+ "Some Maven plugins may not work when importing projects or updating source folders."
+				+ "5/2/12 1:33:47 PM CST: Updating index central|http://repo1.maven.org/maven2"
+				+ "5/2/12 1:33:51 PM CST: Downloading d9d714e11cb097b3ffcec91cccc65d3e : nexus-maven-repository-index.properties"
+				+ "5/2/12 1:33:51 PM CST: Downloaded Repository[d9d714e11cb097b3ffcec91cccc65d3e|http://repo1.maven.org/maven2/.index]/nexus-maven-repository-index.properties"
+				+ "5/2/12 1:35:27 PM CST: Unable to download Repository[d9d714e11cb097b3ffcec91cccc65d3e|http://repo1.maven.org/maven2/.index]/nexus-maven-repository-index.gz: java.io.IOException: The server did not respond within the configured timeout."
+				+ "5/2/12 1:35:27 PM CST: Unable to update index for central|http://repo1.maven.org/maven2"
+				+ "5/2/12 1:47:47 PM CST: Maven Builder: AUTO_BUILD"
+				+ "5/2/12 1:51:33 PM CST: Maven Builder: AUTO_BUILD"
+				+ "5/2/12 1:54:57 PM CST: Maven Builder: AUTO_BUILD ";
+		assertEquals(961, fileContents.length());
+		byte[] buffer = fileContents.getBytes();
+
+		// 将961字节的内容写入文件
+		assertEquals(961, file.fwrite(buffer, 1, buffer.length, fileObject1));
+		//移动文件位置指针
+		assertEquals(0, file.fseek(fileObject1, 0, (int)CRFile.SEEK_SET));
+
+		//关闭文件对象
+		assertEquals(0, file.fclose(fileObject1));
+		//移动文件位置指针
+		assertEquals(CRFile.EOF,file.fseek(fileObject1, 0, (int)CRFile.SEEK_SET));
+		//关闭文件对象之后读文件
+		buffer = new byte[10];
+		assertEquals(CRFile.EOF, file.fread(buffer, 1, buffer.length, fileObject1));
+		//读取文件位置指针值
+		assertEquals(CRFile.EOF, file.ftell(fileObject1));
+		//往文件写内容
+		buffer = fileContents.substring(0, 10).getBytes();
+		assertEquals(CRFile.EOF, file.fwrite(buffer, 1, buffer.length, fileObject1));
+		//读取文件位置指针值
+		assertEquals(CRFile.EOF, file.ftell(fileObject1));
+	}
 	// 删除文件测试
 	public void test_File_Remove() {
 		// format the disk and create a file object
@@ -331,12 +382,15 @@ public class CRFileTest extends TestCase {
 		assertEquals(961, file.fwrite(buffer, 1, buffer.length, fileObject1));
 		// 查看文件位置指针
 		assertEquals(961, file.ftell(fileObject1));
-
-		// 删除文件
+		
+		//删除一不存在的文件
+		assertEquals(CRFile.EOF, file.remove("file2"));
+		
+		// 删除存在的文件
 		assertEquals(0, file.remove("file1"));
-		assertEquals(0, file.fclose(fileObject1));
-		assertEquals(CRFile.EOF, file.remove("file1"));
 
+
+		//删除文件之后读写文件失败
 		// 读取文件失败
 		buffer = new byte[fileContents.length()];
 		assertEquals(-1, file.fread(buffer, 1, buffer.length, fileObject1));
@@ -348,6 +402,9 @@ public class CRFileTest extends TestCase {
 		fileObject1 = (CRFile) file.fopen("file1", "r");
 		assertNull(fileObject1);
 
+		//再一次删除
+		assertEquals(CRFile.EOF, file.remove("file1"));
+		
 		// 再一次创建文件
 		fileObject1 = (CRFile) file.fopen("file1", "w");
 		assertEquals(0, file.ftell(fileObject1));
@@ -426,7 +483,7 @@ public class CRFileTest extends TestCase {
 	}
 
 	// 在文件的任意位置读
-	public void test_File_ReadFileAtAnyLocation() {
+	public void test_File_ReadAtAnyLocationOfFile() {
 		// format the disk and create a file object
 		assertEquals(0, fs.format(true, 512));
 		// 创建文件"file1"
@@ -506,10 +563,13 @@ public class CRFileTest extends TestCase {
 		// 此时读取的是整个文件的内容
 		assertTrue(961 == file.fread(buffer, 1, buffer.length, fileObject1));
 		assertTrue(fileContents.equals(new String(buffer, 0, 961)));
+		
+		// 关闭文件
+		assertEquals(0, file.fclose(fileObject1));
 	}
 
 	// 在文件的任意位置写
-	public void test_File_WriteFileAtAnyLocation() {
+	public void test_File_WriteAtAnyLocationOfFile() {
 		// format the disk and create a file object
 		assertEquals(0, fs.format(true, 512));
 		// 创建文件"file1"
